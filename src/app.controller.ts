fdb0cd347d77baefab90fc7c3d31756208890010
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2020-11-18 13:01:15 +0700

    Add custom service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @GrpcMethod('CustomHelloService', 'GetCustomHello')
+  getCustomHello(data: string) {
+    return { reply: `Hello ${data}!` };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
+const logger = new Logger();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'customHello',
+        protoPath: join(__dirname, './protos/custom-hello.proto'),
+      },
+    },
+  );
+
+  await app.listen(() => logger.log('Custom Hello service is listening...'));
 }
+
 bootstrap();
